2d80a0cc44f9c315cf513541647f16f99cd60fb7
Rocky <993916778@qq.com>
2023-10-10 09:24:32 +0800

    feat: 封装报文测试

--- rockyrpc-demo/src/test/java/com/rocky/nettyTest.java
+++ rockyrpc-demo/src/test/java/com/rocky/nettyTest.java
@@ -1,9 +1,16 @@
 package com.rocky;
 
+import com.rocky.netty.AppClient;
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
 import io.netty.buffer.CompositeByteBuf;
 import io.netty.buffer.Unpooled;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.charset.StandardCharsets;
 
 public class nettyTest {
 
@@ -15,5 +22,43 @@ public class nettyTest {
         CompositeByteBuf byteBuf = Unpooled.compositeBuffer();
         byteBuf.addComponents(header, body);
 
+    }
+
+    @Test
+    public void testMessage() throws IOException {
+        ByteBuf message = Unpooled.buffer();
+        message.writeBytes("ydl".getBytes(StandardCharsets.UTF_8));
+        message.writeByte(1);
+        message.writeShort(125);
+        message.writeInt(256);
+        message.writeByte(1);
+        message.writeByte(0);
+        message.writeByte(2);
+        message.writeLong(251455L);
+
+        // 用对象流转化为字节数据
+        AppClient appClient = new AppClient();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(outputStream);
+        oos.writeObject(appClient);
+        byte[] bytes = outputStream.toByteArray();
+        message.writeBytes(bytes);
+
+        printAsBinary(message);
+
+    }
+    public static void printAsBinary(ByteBuf byteBuf) {
+        byte[] bytes = new byte[byteBuf.readableBytes()];
+        byteBuf.getBytes(byteBuf.readerIndex(), bytes);
+
+        String binaryString = ByteBufUtil.hexDump(bytes);
+        StringBuilder formattedBinary = new StringBuilder();
+
+        for (int i = 0; i < binaryString.length(); i += 2) {
+            formattedBinary.append(binaryString.substring(i, i + 2)).append(" ");
+        }
+
+        System.out.println("Binary representation: " + formattedBinary.toString());
     }
 }
